c4c94950454c92e3199957280ce64b55dd607829
GiaIn <yenbg@viettinlab.com>
2023-07-06 13:45:23 +0700

    add report container

--- KBHM/src/Components/Modal.pdf.js
+++ KBHM/src/Components/Modal.pdf.js
@@ -1,52 +1,44 @@
-import { Modal } from 'antd';
-import React, { useEffect, useState } from 'react';
+import { Modal } from "antd";
+import React, { useEffect, useState } from "react";
 // Core viewer
-import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { Viewer, Worker } from "@react-pdf-viewer/core";
 
 // Plugins
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 // Import styles
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
-const PDfViewer = ({ DataPDF, Open, onCancel }) => {
+const PDfViewer = ({ DataPDF, Open, onCancel }) => {1
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const base64toBlob = () => {
+    // Cut the prefix `data:application/pdf;base64` from the raw base 64
+    console.log(DataPDF);
+    const base64WithoutPrefix = DataPDF;
+    const bytes = atob(base64WithoutPrefix);
+    let length = bytes.length;
+    let out = new Uint8Array(length);
 
-    const [urlPDf, SetUrlPdf] = useState();
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-    const base64toBlob = ({ data }) => {
-        // Cut the prefix `data:application/pdf;base64` from the raw base 64
-        const base64WithoutPrefix = data;
-        const bytes = atob(base64WithoutPrefix);
-        let length = bytes.length;
-        let out = new Uint8Array(length);
+    while (length--) {
+      out[length] = bytes.charCodeAt(length);
+    }
 
-        while (length--) {
-            out[length] = bytes.charCodeAt(length);
-        }
+    const blob = Blob([out], { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+    console.log(url);
+    return url;
+  };
 
-        return new Blob([out], { type: 'application/pdf' });
-    };
-    useEffect(() => {
-        if (DataPDF) {
-            const blob = base64toBlob({ data: DataPDF });
-            const url = URL.createObjectURL(blob);
-            SetUrlPdf(url)
-        }
-
-    }, [DataPDF])
-
-    return (
-        <Modal open={Open} onCancel={onCancel} width={1000  +'px'}>
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                <Viewer
-                    fileUrl={urlPDf}
-                    plugins={[
-                        defaultLayoutPluginInstance,
-                    ]}
-                />
-            </Worker>
-        </Modal>
-    );
-}
-export default PDfViewer
+  return (
+    <Modal open={Open} onCancel={onCancel} width={1000 + "px"}>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <Viewer
+          fileUrl={base64toBlob}
+          plugins={[defaultLayoutPluginInstance]}
+        />
+      </Worker>
+    </Modal>
+  );
+};
+export default PDfViewer;
