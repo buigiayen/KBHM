aa9bed0babfe82ebd0a0573233de206220d740b5
kienntviettinlab <kien.nt@viettinlab.com>
2024-10-13 01:40:53 +0700

    Update

--- KBHM/src/Data/Api/DangKyKham.js
+++ KBHM/src/Data/Api/DangKyKham.js
@@ -92,7 +92,7 @@ export const POST_SyncDonor = async (ID) => {
         ElementID: LoaiHienThanhPhan,
         BLOODPRESSURE: HuyetAp,
         HGB: "1",
-        PULSE: Mach,
+        PULSE: Mach ? `${Mach}` : null,
         STATUS: TinhTrangLamSang,
         WEIGH: CanNang,
         HEIGH: ChieuCao,
